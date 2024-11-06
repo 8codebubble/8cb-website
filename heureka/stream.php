89ff819710d5c3e27ba9eac5dbdc0f8ab172d0f4
8codebubble-pub <8codebubble@gmail.com>
2024-11-07 07:49:08 +0900

    Publii: update content

---
+++ heureka/stream.php
@@ -0,0 +1,211 @@
+<?php
+header('content-type: application/json; charset=utf-8');
+header("access-control-allow-origin: *");
+?>
+[{
+	"id": "1",
+	"question": "I will send to you a postcard. Or I will send you a postcard.",
+	"answer": "I will send you a postcard.",
+	"word": "postcard"
+}, {
+	"id": "2",
+	"question": "To these, he answered. Or To these words, he answered.",
+	"answer": "To these words, he answered",
+	"word": "answered"
+}, {
+	"id": "4",
+	"question": "Holidays begin on Friday. Or Holidays begin from Friday.",
+	"answer": "Holidays begin on Friday.",
+	"word": "holidays"
+}, {
+	"id": "5",
+	"question": "The Wednesday is meeting day at our company. Or Wednesday is meeting day at our company.",
+	"answer": "Wednesday is meeting day at our company",
+	"word": "wednesday"
+}, {
+	"id": "6",
+	"question": "Matthew wanted to make an impression on Amy. Or Matthew wanted to make impression on Amy.",
+	"answer": "Matthew wanted to make an impression on Amy.",
+	"word": "impression"
+}, {
+	"id": "7",
+	"question": "Do you like to go cinema? Or Do you like to go to the cinema?",
+	"answer": "Do you like to go to the cinema?",
+	"word": "cinema"
+}, {
+	"id": "8",
+	"question": "Jane considers me her best friend. Or Jane considers me as her best friend.",
+	"answer": "Jane considers me her best friend.",
+	"word": "considers"
+}, {
+	"id": "9",
+	"question": "John stepped outside of the building for fresh air. Or John stepped outside for fresh air.",
+	"answer": "John stepped outside the building for fresh air.",
+	"word": "building"
+}, {
+	"id": "10",
+	"question": "I have not received a reply to my letter yet. Or I have not received a reply my letter yet.",
+	"answer": "I have not received a reply to my letter yet.",
+	"word": "received"
+}, {
+	"id": "11",
+	"question": "Shelley has finished the paperback. Or Shelley has finished from the paperback.",
+	"answer": "Shelley has finished the paperback.",
+	"word": "finished"
+}, {
+	"id": "12",
+	"question": "Cats are independent animals. Or The cats are independent animals.",
+	"answer": "Cats are independent animals.",
+	"word": "independent"
+}, {
+	"id": "13",
+	"question": "These shoes fit better than those. Or These shoes fit more perfectly than those.",
+	"answer": "These shoes fit better than those.",
+	"word": "perfectly"
+}, {
+	"id": "14",
+	"question": "She is eleven years. Or She is eleven years old.",
+	"answer": "She is eleven years old.",
+	"word": "eleven"
+}, {
+	"id": "15",
+	"question": "Ann finally got a work in a company. Or Ann finally got work in a company.",
+	"answer": "Ann finally got work in a company.",
+	"word": "finally"
+}, {
+	"id": "16",
+	"question": "The Shakespeare's comedies are often performed in Stratford. Or Shakespeare's comedies are often performed in Stratford.",
+	"answer": "Shakespeare's comedies are often performed in Stratford.",
+	"word": "performed"
+}, {
+	"id": "17",
+	"question": "My brother does not resemble anyone in the family. Or My brother does not resemble anyone else in the family.",
+	"answer": "My brother does not resemble anyone in the family.",
+	"word": "resemble"
+}, {
+	"id": "18",
+	"question": "It is dangerous to approach a hungry animal. Or It is dangerous to be approaching a hungry animal.",
+	"answer": "It is dangerous to approach a hungry animal.",
+	"word": "dangerous"
+}, {
+	"id": "22",
+	"question": "Be careful not to make mistake. Or Be careful not to make a mistake.",
+	"answer": "Be careful not to make a mistake.",
+	"word": "mistake"
+}, {
+	"id": "23",
+	"question": "You remind me of my sister. Or You remind me from my sister.",
+	"answer": "You remind me of my sister.",
+	"word": "remind"
+}, {
+	"id": "24",
+	"question": "Carl is wiser than mark. Or Carl is more wiser than mark.",
+	"answer": "Carl is wiser than mark.",
+	"word": "wiser"
+}, {
+
+	"id": "3",
+	"question": "The officer explained to me why he stopped the car. Or the officer explained me why he stopped the car.",
+	"answer": "The officer explained to me why he stopped the car.",
+	"word": "officer"
+}, {
+	"id": "19",
+	"question": "They searched the missing child all night. Or They searched for the missing child all night.",
+	"answer": "They searched for the missing child all night.",
+	"word": "searched"
+}, {
+	"id": "20",
+	"question": "The injured woman was crying for help. Or The injured was crying for help.",
+	"answer": "The injured woman was crying for help.",
+	"word": "injured"
+}, {
+	"id": "21",
+	"question": "Better to go to sleep early. Or You had better go to sleep early.",
+	"answer": "you had better go to sleep early.",
+	"word": "sleep"
+}, {
+	"id": "25",
+	"question": "Jason regards me as his brother. Or Jason regards me his brother.",
+	"answer": "Jason regards me as his brother.",
+	"word": "regards"
+}, {
+
+	"id": "26",
+	"question": "For this reason, the road will be closed on Sunday. Or For this, the road will be closed on Sunday.",
+	"answer": "For this reason the road will be closed on Sunday.",
+	"word": "reason"
+}, {
+	"id": "27",
+	"question": "It is impossible to make her see the truth. Or It is impossible to make her to see the truth",
+	"answer": "It is impossible to make her see the truth.",
+	"word": "impossible"
+}, {
+	"id": "28",
+	"question": "Jack waited behind the door. Or Jack waited behind of the door.",
+	"answer": "Jack waited behind the door.",
+	"word": "waited"
+}, {
+
+	"id": "29",
+	"question": "Be careful not to make mistake. Or Be careful not to make a mistake.",
+	"answer": "Be careful not to make a mistake.",
+	"word": "mistake"
+}, {
+	"id": "30",
+	"question": "The professor I know him only slightly. Or I know the professor only slightly.",
+	"answer": "I know the professor only slightly.",
+	"word": "slightly"
+}, {
+	"id": "31",
+	"question": "Mary said he went to NY on a business trip.Or Mary said that he went to NY on a business trip.",
+	"answer": "Mary said he went to NY on a business trip.",
+	"word": "business"
+}, {
+
+	"id": "32",
+	"question": "The apple, which I ate, was sour. Or The apple, which I ate, it was sour",
+	"answer": " The apple, which I ate, was sour.",
+	"word": "sour"
+}, {
+	"id": "33",
+	"question": "Lee has auburn hair. Or Lee has got auburn hair",
+	"answer": "Lee has auburn hair.",
+	"word": "auburn"
+}, {
+	"id": "34",
+	"question": "My daughter shared with me her ice cream. Or My daughter shared me her ice cream",
+	"answer": "My daughter shared with me her ice cream.",
+	"word": "daughter"
+}, {
+
+	"id": "35",
+	"question": "Lynn reached at the party later than I did. Or Lynn reached the party later than I did.",
+	"answer": "Lynn reached the party later than I did.",
+	"word": "reached"
+}, {
+	"id": "36",
+	"question": "From now on I will not drive over the speed limit. Or from now and on I will not drive over the speed limit",
+	"answer": "From now on I will not drive over the speed limit.",
+	"word": "limit"
+}, {
+	"id": "37",
+	"question": "I am so excited so that I cannot sleep. Or I am so excited that I cannot sleep",
+	"answer": "I am so excited that I cannot sleep.",
+	"word": "excited"
+
+}, {
+	"id": "38",
+	"question": "The Malaria is still a common disease. Or Malaria is still a common disease.",
+	"answer": "Malaria is still a common disease.",
+	"word": "disease"
+}, {
+	"id": "39",
+	"question": "Tracy returned to work after her baby was born. Or Tracy returned back to work after her baby was born",
+	"answer": "Tracy returned to work after her baby was born.",
+	"word": "returned"
+}, {
+	"id": "40",
+	"question": "Joe write English well. Or Joe writes English well.",
+	"answer": "Joe writes English well.",
+	"word": "English"
+}]
